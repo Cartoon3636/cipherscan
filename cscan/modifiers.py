953a4086a52db70d4e7af1c8ab54c9b65eb31e48
Hubert Kario <hubert@kario.pl>
2016-07-26 22:55:34 +0200

    convert modifiers to use the callback mechanism
    make the modifiers use callback mechanism and more efficient
    algorithms in few places

--- cscan/modifiers.py
+++ cscan/modifiers.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from tlslite.constants import CipherSuite
 from tlslite.extensions import SNIExtension, PaddingExtension
+import itertools
 
 def patch_call(instance, func):
     class _(type(instance)):
@@ -10,14 +11,11 @@ def patch_call(instance, func):
 
 
 def no_sni(generator):
-    def ret_fun(self, hostname):
-        ret = super(type(self), self).__call__(hostname)
-        ret.extensions = [i for i in ret.extensions
-                          if not isinstance(i, SNIExtension)]
-        return ret
-    patch_call(generator, ret_fun)
-    generator.modifications += ["no SNI"]
+    generator.extensions[:] = (x for x in generator.extensions
+                               if not isinstance(x, SNIExtension))
+    generator.modifications.append("no SNI")
     return generator
+
 
 proto_versions = {(3, 0): "SSLv3",
                   (3, 1): "TLSv1.0",
@@ -28,15 +26,18 @@ proto_versions = {(3, 0): "SSLv3",
                   (3, 6): "TLSv1.5"}
 
 
+def version_to_str(version):
+    version_name = proto_versions.get(version, None)
+    if version_name is None:
+        version_name = "{0[0]}.{0[1]}".format(version)
+    return version_name
+
+
 def set_hello_version(generator, version):
     """Set client hello version"""
 
     generator.version = version
-
-    version_name = proto_versions.get(version, None)
-    if version_name is None:
-        version_name = "{0[0]}.{0[1]}".format(version)
-    generator.modifications += [version_name]
+    generator.modifications += [version_to_str(version)]
     return generator
 
 
@@ -44,42 +45,38 @@ def set_record_version(generator, version):
     """Set record version, un-SSLv2-ify"""
 
     generator.record_version = version
-    generator.ciphers = [i for i in generator.ciphers if i <= 0xffff]
-
-    def ret_fun(self, hostname):
-        ret = super(type(self), self).__call__(hostname)
-        ret.ssl2 = False
-        return ret
-    patch_call(generator, ret_fun)
-
-    version_name = proto_versions.get(version, None)
-    if version_name is None:
-        version_name = "{0[0]}.{0[1]}".format(version)
-    generator.modifications += ["r/{0}".format(version_name)]
+    generator.ciphers[:] = (i for i in generator.ciphers if i <= 0xffff)
+    generator.ssl2 = False
+    generator.modifications += ["r/{0}".format(version_to_str(version))]
     return generator
 
 
 def no_extensions(generator):
     """Remove extensions"""
 
-    def ret_fun(self, hostname):
-        ret = super(type(self), self).__call__(hostname)
-        ret.extensions = None
-        return ret
-    patch_call(generator, ret_fun)
+    generator.extensions = None
     generator.modifications += ["no ext"]
     return generator
+
+
+def divceil(divident, divisor):
+    quot, r = divmod(divident, divisor)
+    return quot + int(bool(r))
 
 
 def truncate_ciphers_to_size(generator, size):
     """Truncate list of ciphers until client hello is no bigger than size"""
 
-    def ret_fun(self, hostname, size=size):
-        ret = super(type(self), self).__call__(hostname)
-        while len(ret.write()) > size:
-            ret.cipher_suites.pop()
-        return ret
-    patch_call(generator, ret_fun)
+    def cb_fun(client_hello, size=size):
+        hello_len = len(client_hello.write())
+        bytes_to_remove = hello_len - size
+        if bytes_to_remove > 0:
+            ciphers_to_remove = divceil(bytes_to_remove, 2)
+            client_hello.cipher_suites[:] = \
+                    client_hello.cipher_suites[:-ciphers_to_remove]
+        return client_hello
+
+    generator.callbacks.append(cb_fun)
     generator.modifications += ["trunc c/{0}".format(size)]
     return generator
 
@@ -93,22 +90,22 @@ def append_ciphers_to_size(generator, size):
     it (0x5600, a.k.a TLS_FALLBACK_SCSV, excluded)
     """
 
-    def ret_fun(self, hostname, size=size):
-        ret = super(type(self), self).__call__(hostname)
+    def cb_fun(client_hello, size=size):
         ciphers_iter = iter(range(0x2000, 0xc000))
-        ciphers_present = set(ret.cipher_suites)
-        bytes_to_add = size - len(ret.write())
-        while bytes_to_add > 0:
-            ciph = next(ciphers_iter)
-            # don't put ciphers with special meaning or already present
-            if ciph == CipherSuite.TLS_FALLBACK_SCSV or \
-               ciph in ciphers_present:
-                continue
-            ciphers_present.add(ciph)
-            ret.cipher_suites.append(ciph)
-            bytes_to_add -= 2
-        return ret
-    patch_call(generator, ret_fun)
+        ciphers_present = set(client_hello.cipher_suites)
+        # we don't want to add a cipher id with special meaning
+        # and the set is used only internally
+        ciphers_present.add(CipherSuite.TLS_FALLBACK_SCSV)
+
+        bytes_to_add = size - len(client_hello.write())
+        if bytes_to_add > 0:
+            ciphers_to_add = divceil(bytes_to_add, 2)
+            ciphers_gen = (x for x in ciphers_iter
+                           if x not in ciphers_present)
+            client_hello.cipher_suites.extend(itertools.islice(ciphers_gen,
+                                                               ciphers_to_add))
+        return client_hello
+    generator.callbacks.append(cb_fun)
     generator.modifications += ["append c/{0}".format(size)]
     return generator
 
@@ -121,21 +118,22 @@ def extend_with_ext_to_size(generator, size):
     the specified size is reached
     """
 
-    def ret_fun(self, hostname, size=size):
-        ret = super(type(self), self).__call__(hostname)
-        if len(ret.write()) > size:
-            return ret
-        if not ret.extensions:
-            ret.extensions = []
-        ext = next((x for x in ret.extensions
+    def cb_fun(client_hello, size=size):
+        if len(client_hello.write()) > size:
+            return client_hello
+        if not client_hello.extensions:
+            client_hello.extensions = []
+        ext = next((x for x in client_hello.extensions
                     if isinstance(x, PaddingExtension)), None)
         if not ext:
             ext = PaddingExtension()
-            ret.extensions.append(ext)
-        bytes_to_add = size - len(ret.write())
+            client_hello.extensions.append(ext)
+        # check if just adding the extension, with no payload, haven't pushed
+        # us over the limit
+        bytes_to_add = size - len(client_hello.write())
         if bytes_to_add > 0:
             ext.paddingData += bytearray(bytes_to_add)
-        return ret
-    patch_call(generator, ret_fun)
+        return client_hello
+    generator.callbacks.append(cb_fun)
     generator.modifications += ["append e/{0}".format(size)]
     return generator
